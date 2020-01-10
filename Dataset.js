023e15e6e968ca259eec3a64c639702d2163e018
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-10 11:47:23 +0530

    Query Of DataSet Completed

--- Dataset.js
+++ Dataset.js
@@ -5,11 +5,13 @@ const obj = csv();
 var myData = [];
 let distance = [];
 let distance2 = [];
-let GroupByData = []
-let totalTrain = []
-let myDataUnique = []
+let GroupByData = [];
+let totalTrain = [];
+let myDataUnique = [];
+let findTrain = [];
 
 function MyCSV(Fone, Ftwo, Fthree, Ffour, Ffive, Fsix, Fseven, Feight, Fnine, Ften, Feleven)
+
             {
                 this.FieldOne = Fone;
                 this.FieldTwo = Ftwo;
@@ -40,13 +42,17 @@ obj.from.path('./isl_wise_train_detail_03082015_v1.csv').to.array(function (data
     myData.forEach((items) => {
         distance.push(items.FieldEight)
     })
+
+    myData.forEach((items) => {
+        findTrain.push(items.FieldOne)
+    })
     // console.log(myData)
 //start Distance
 
-    myData.forEach((items) => {
-        distance2.push(items.FieldOne)
-    })
-// Distance 2 over
+    // myData.forEach((items) => {
+    //     distance2.push(items.FieldOne)
+    // })
+// Distance 2 over 
 
 
     let final = distance.slice(1).map(function (x) {
@@ -56,64 +62,83 @@ obj.from.path('./isl_wise_train_detail_03082015_v1.csv').to.array(function (data
     console.log('Maximum Distance Is : ' + Math.max.apply(Math, final))
     console.log('Minimum Distance Is : ' +  Math.min.apply(Math, final) + '000')
 
-    //GroupBy
+    //GroupBy DataSet Question : 3
     
     var groupBy = function(xs, key) {
         return xs.reduce(function(rv, x) {
-            console.log('----',rv,x);
+            // console.log('----',rv,x);
           (rv[x[key]] = rv[x[key]] || []).push(x);
-          console.log('+++++++',rv);
+        //   console.log('+++++++',rv);
           return rv;
         }, {});
       };
 
     //   for(let i=0 ; i<= 13, i++)
-    const arr = [
-        {
-            a:'x1',
-            b:'y1',
-        },
-        {
-            a:'x2',
-            b:'y2',
-        },
-        {
-            a:'x2',
-            b:'y3',
-        },
-        {
-            a:'x4',
-            b:'y4',
-        },
-    ]
-    console.log(groupBy(arr,'a'))
+    // const arr = [
+    //     {
+    //         a:'x1',
+    //         b:'y1',
+    //     },
+    //     {
+    //         a:'x2',
+    //         b:'y2',
+    //     },
+    //     {
+    //         a:'x2',
+    //         b:'y3',
+    //     },
+    //     {
+    //         a:'x4',
+    //         b:'y4',
+    //     },
+    // ]
+    // for(let j=1;j<=7;j++){
+         console.log(groupBy(myData,'FieldThree')[1],groupBy(myData,'FieldThree')[2],groupBy(myData,'FieldThree')[3],
+         groupBy(myData,'FieldThree')[4],groupBy(myData,'FieldThree')[5],groupBy(myData,'FieldThree')[6]),
+         groupBy(myData,'FieldThree')[7]
+    // }
      // var yy = GroupByData.push(groupBy(myData,'FieldThree')[3]);
       
 
 
 
     // myData.forEach((items) => {
-    //     totalTrain.push(items.FieldOne); 
+    //     console.log(items.FieldOne);
     // })
-    // console.log(totalTrain);
+    
 
 
-////
 
 
-    // function removeDuplicates(FieldOne) {
-        
-//         var lookupObject  = {};
-   
-//         for(var i in myData) {
-//            lookupObject[myData[i][prop]] = myData[i];
-//         }
-   
-//         for(i in lookupObject) {
-//             myDataUnique.push(lookupObject[i]);
-//         }
-//          return myDataUnique;
-//     }
+
+// Romove Duplication Of array !! Qustn: 1
+
+
+    function getUnique(array){
+        let uniqueArray = [];
+        // Loop through array values
+
+        for(let value of array){
+
+            if(uniqueArray.indexOf(value) === -1){
+
+                uniqueArray.push(value);       
+                // console.log(uniqueArray)
+            
+            }
+        }
+
+        return uniqueArray;
+        // console.log(uniqueArray)
+
+
+
+    }
+
+    var uniqueNames = getUnique(findTrain);
+    console.log(uniqueNames)
+    console.log(uniqueNames.length);
+
    
 //    var uniqueArray = removeDuplicates(myData, "Train No.");
 //    console.log("uniqueArray is: " + JSON.stringify(uniqueArray));
