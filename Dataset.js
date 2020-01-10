497c1e9dfca8c3c9012e213becae5fdd645c9648
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-10 16:09:55 +0530

    Update All Questions Solution

--- Dataset.js
+++ Dataset.js
@@ -1,36 +1,33 @@
-const csv = require('csv');
+const Table = require('cli-table')
+const process = require('process')
 const fs = require('fs');
+const csv = require('csv');
 const obj = csv();
 
-var myData = [];
-let distance = [];
-let distance2 = [];
-let GroupByData = [];
-let totalTrain = [];
-let myDataUnique = [];
-let findTrain = [];
-
-function MyCSV(Fone, Ftwo, Fthree, Ffour, Ffive, Fsix, Fseven, Feight, Fnine, Ften, Feleven)
-
-            {
-                this.FieldOne = Fone;
-                this.FieldTwo = Ftwo;
-                this.FieldThree = Fthree;
-                this.FieldFour = Ffour;
-                this.FieldFive = Ffive;
-                this.FieldSix = Fsix;
-                this.FieldSeven = Fseven;
-                this.FieldEight = Feight;
-                this.FieldNine = Fnine;
-                this.FieldTen = Ften;
-                this.FieldEleven = Feleven;
-            }
+let myData = [], distance = [], GroupByData = [], totalTrain = [], findTrain = [], trainStop = [], stopLength = [];
 
 
-// fs.readFile('./isl_wise_train_detail_03082015_v1.csv', (err, data)=>{
+// var table = new Table({
+//     head: ['Train Number', 'Train name', 'islno', 'station Code', 'Station Name', 'Arrival time', 'Departure time', 'Distance', 'Source Station Code', 'source Station Name', 'Destination station Code', 'Destination Station Name']
+//     , colWidths: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+// });
 
-//     // Find a total number Of Trains
-//     console.log(data.toString().length);
+function MyCSV(Fone, Ftwo, Fthree, Ffour, Ffive, Fsix, Fseven, Feight, Fnine, Ften, Feleven) {
+    this.Train_No = Fone;
+    this.Train_name = Ftwo;
+    this.islno = Fthree;
+    this.station_code = Ffour;
+    this.station_Name = Ffive;
+    this.Arrival_time = Fsix;
+    this.Deprature_time = Fseven;
+    this.Distanceoftrain = Feight;
+    this.Source_station_code = Fnine;
+    this.Destination_station_code = Ften;
+    this.Destination_station_name = Feleven;
+    // table.push([Fone, Ftwo, Fthree, Ffour, Ffive, Fsix, Fseven, Feight, Fnine, Ften, Feleven])
+}
+
+// Find Min Max Length of Distance
 
 obj.from.path('./isl_wise_train_detail_03082015_v1.csv').to.array(function (data) {
 
@@ -38,113 +35,79 @@ obj.from.path('./isl_wise_train_detail_03082015_v1.csv').to.array(function (data
         myData.push(new MyCSV(data[index][0], data[index][1], data[index][2], data[index][3], data[index][4], data[index][5], data[index][6], data[index][7], data[index][8], data[index][9], data[index][10], data[index][11]
         ))
     };
+    console.log(table.toString());
 
     myData.forEach((items) => {
-        distance.push(items.FieldEight)
+        distance.push(items.Distanceoftrain)
     })
 
     myData.forEach((items) => {
-        findTrain.push(items.FieldOne)
+        findTrain.push(items.Train_No)
     })
-    // console.log(myData)
-//start Distance
-
-    // myData.forEach((items) => {
-    //     distance2.push(items.FieldOne)
-    // })
-// Distance 2 over 
 
 
     let final = distance.slice(1).map(function (x) {
         return parseInt(x, 10)
     });
 
+
     console.log('Maximum Distance Is : ' + Math.max.apply(Math, final))
-    console.log('Minimum Distance Is : ' +  Math.min.apply(Math, final) + '000')
+    console.log('Minimum Distance Is : ' + Math.min.apply(Math, final) + '000')
 
-    //GroupBy DataSet Question : 3
-    
-    var groupBy = function(xs, key) {
-        return xs.reduce(function(rv, x) {
-            // console.log('----',rv,x);
-          (rv[x[key]] = rv[x[key]] || []).push(x);
-        //   console.log('+++++++',rv);
-          return rv;
-        }, {});
-      };
+// if(process.argv[2] === 1)
+    // Romove Duplication Of array !! Qustn: 1
 
-    //   for(let i=0 ; i<= 13, i++)
-    // const arr = [
-    //     {
-    //         a:'x1',
-    //         b:'y1',
-    //     },
-    //     {
-    //         a:'x2',
-    //         b:'y2',
-    //     },
-    //     {
-    //         a:'x2',
-    //         b:'y3',
-    //     },
-    //     {
-    //         a:'x4',
-    //         b:'y4',
-    //     },
-    // ]
-    // for(let j=1;j<=7;j++){
-         console.log(groupBy(myData,'FieldThree')[1],groupBy(myData,'FieldThree')[2],groupBy(myData,'FieldThree')[3],
-         groupBy(myData,'FieldThree')[4],groupBy(myData,'FieldThree')[5],groupBy(myData,'FieldThree')[6]),
-         groupBy(myData,'FieldThree')[7]
-    // }
-     // var yy = GroupByData.push(groupBy(myData,'FieldThree')[3]);
-      
-
-
-
-    // myData.forEach((items) => {
-    //     console.log(items.FieldOne);
-    // })
-    
-
-
-
-
-
-// Romove Duplication Of array !! Qustn: 1
-
-
-    function getUnique(array){
+    function getUnique(array) {
         let uniqueArray = [];
         // Loop through array values
 
-        for(let value of array){
+        for (let value of array) {
 
-            if(uniqueArray.indexOf(value) === -1){
-
-                uniqueArray.push(value);       
-                // console.log(uniqueArray)
-            
+            if (uniqueArray.indexOf(value) === -1) {
+                uniqueArray.push(value);
+                // console.log(uniqueArray)   
             }
         }
-
         return uniqueArray;
         // console.log(uniqueArray)
-
-
-
     }
 
-    var uniqueNames = getUnique(findTrain);
-    console.log(uniqueNames)
-    console.log(uniqueNames.length);
 
-   
-//    var uniqueArray = removeDuplicates(myData, "Train No.");
-//    console.log("uniqueArray is: " + JSON.stringify(uniqueArray));
+    let uniqueNames = getUnique(findTrain);
+    // console.log(uniqueNames)
+
+    console.log("Total number of Trains : " + uniqueNames.length);
 
 
-    
-    
 
+    //GroupBy DataSet Question : 3
+    let groupBy = function (xs, key) {
+        return xs.reduce(function (rv, x) {
+            (rv[x[key]] = rv[x[key]] || []).push(x);
+            return rv;
+        }, {});
+    };
+    for (let j = 1; j <= 8; j++) {
+        console.log(groupBy(myData, 'islno')[j]);
+    }
+
+
+    var groupByTrain = function (xs, key) {
+        return trainStop.push(xs.reduce(function (rv, x) {
+            let cnt = 0;
+            (rv[x[key]] = rv[x[key]] || []).push(cnt = cnt + 1);
+            return rv;
+        }, {}));
+    };
+
+    groupByTrain(myData, 'Train_name');
+    Object.keys(trainStop[0]).forEach(items => {
+        stopLength.push(trainStop[0][items].length);
+    });
+
+    console.log(myData[stopLength.indexOf(Math.max.apply(Math, stopLength))], "       Maximum Stops:" + Math.max.apply(Math, stopLength));
+
+    console.log('\n');
+
+    console.log(myData[stopLength.indexOf(Math.min.apply(Math, stopLength))], "       Minimum Stops:" + Math.min.apply(Math, stopLength));
 });
