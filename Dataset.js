09f2581ac7c4eaa42ad1b77cdf61de41f578dc18
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-17 09:54:22 +0530

    Switch case Added

--- Dataset.js
+++ Dataset.js
@@ -1,17 +1,19 @@
 const csv = require('csv');
 const process = require('process');
 const Table = require('cli-table');
-const obj = csv();
+const Obj = csv();
 
 
 let table = new Table({
-   head: ['Train No.', 'train Name', 'islno,station Code', 'Station Name', 'Arrival time', 'Departure time', 'Distance', 'Source Station Code', 'source Station Name', 'Destination station Code', 'Destination Station Name']
+   head: ['Train No.', 'train Name', 'islno,station Code', 'Station Name', 'Arrival time', 
+   'Departure time', 'Distance', 'Source Station Code', 'source Station Name', 'Destination station Code', 'Destination Station Name']
    , colWidths: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 });
 
-let MyData = [],finalDitance = [], distance = [],  trainStop = [], stopLength = [], findTrain = [];
+let myData = [],finalDitance = [], distance = [],  trainStop = [], stopLength = [], findTrain = [];
 
-function MyCSV(data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11) {
+function MyCSV(data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11) 
+{
     this.trainno = data0,
     this.trainname = data1,
     this.islno = data2,
@@ -74,53 +76,58 @@ function minDistance(arrMinDistance) {
    console.log(JSON.stringify(arrMinDistance));
 }
 
+
 function trainData() {
-   obj.from.path('isl_wise_train_detail_03082015_v1.csv').to.array(function (data) {
+   Obj.from.path('isl_wise_train_detail_03082015_v1.csv').to.array(data=> {
 
       for (let index = 0; index < data.length; index++) {
-         MyData.push(new MyCSV(data[index][0], data[index][1], data[index][2],
+         myData.push(new MyCSV(data[index][0], data[index][1], data[index][2],
             data[index][3], data[index][4], data[index][5],
             data[index][6], data[index][7], data[index][8],
             data[index][9], data[index][10], data[index][11]));
-      }
-      if (process.argv[2] === '1') {
-         MyData.forEach(items => {
-            findTrain.push(items.trainname);
-         });         
-         console.log("Total number of Trains : " + getUnique(findTrain));
-         Display(MyData);
-         console.log(table.toString());
-      }
-      else if (process.argv[2] === '2') {
-         Distance(MyData);
-         console.log('Maximum Distance Is : ' + Math.max.apply(Math, finalDistance)) 
-      }
-      else if (process.argv[2] === '3') {
-         Distance(MyData);
-         console.log('Minimum Distance Is : ' + Math.min.apply(Math, finalDistance))
-      }
-      else if (process.argv[2] === '4') {
-         for (let i = 1; i <= 8; i++) {
-            console.log(groupBy(MyData, 'islno')[i]);
-         }
-      }
-      else if (process.argv[2] === '5') {
-         TraingroupBy(MyData, 'trainname');
-         Object.keys(trainStop[0]).forEach(ele => {
-            stopLength.push(trainStop[0][ele].length);
-         });
-         console.log(MyData[stopLength.indexOf(Math.max.apply(Math, stopLength))], "Maximum Stops:" + Math.max.apply(Math, stopLength));
-      }
-      else if (process.argv[2] === '6') {
-         TraingroupBy(MyData, 'trainname');
-         Object.keys(trainStop[0]).forEach(ele => {
-            stopLength.push(trainStop[0][ele].length);
-         });
-         console.log(MyData[stopLength.indexOf(Math.min.apply(Math, stopLength))], "Minimum Stops:" + Math.min.apply(Math, stopLength));
-      }
-      else {
-         console.log("Please Enter a Valid Number Of Choice...!");
-      }
-   });
+			}
+			switch(Number(process.argv[2])){
+				case 1:
+					myData.forEach(items => {
+						 findTrain.push(items.trainname);
+					});         
+					console.log("Total number of Trains : " + getUnique(findTrain));
+					Display(myData);
+					console.log(table.toString());
+					break;
+				case 2:
+					Distance(myData);
+					console.log('Maximum Distance Is : ' + Math.max.apply(Math, finalDistance)) 
+					break;
+				case 3:
+					Distance(myData);
+					console.log('Minimum Distance Is : ' + Math.min.apply(Math, finalDistance))
+				case 4:
+					for (let i = 1; i <= 8; i++) {
+						console.log(groupBy(myData, 'islno')[i]);
+						break;
+				 }
+				case 5:
+					TraingroupBy(myData, 'trainname');
+						Object.keys(trainStop[0]).forEach(ele => 
+							{
+								stopLength.push(trainStop[0][ele].length);
+							});
+							
+					console.log(myData[stopLength.indexOf(Math.max.apply(Math, stopLength))], "Maximum Stops:"
+								+ Math.max.apply(Math, stopLength));
+					break;
+				case 6:
+					TraingroupBy(myData, 'trainname');
+						Object.keys(trainStop[0]).forEach(ele => {
+							stopLength.push(trainStop[0][ele].length);
+							});
+						console.log(myData[stopLength.indexOf(Math.min.apply(Math, stopLength))], "Minimum Stops:"
+						+ Math.min.apply(Math, stopLength));
+					break;
+				default:
+					console.log("Please Enter a Valid Number Of Choice...!");
+	 }
+	})
 }
 trainData();
